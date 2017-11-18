89935b85c6e57146cfbded5d4ecbea17a3b04870
lzz <lzzjingcairensheng@gmail.com>
2017-11-18 15:44:17 +0800

    open smallgame project

---
+++ src/main/java/com/justplay/game/smallgame/SmallgameApplication.java
@@ -0,0 +1,12 @@
+package com.justplay.game.smallgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallgameApplication.class, args);
+	}
+}
